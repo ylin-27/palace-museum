8bed2de485ebe786af04691f3ecf22f0e6ce4308
ylin-27 <260671287@qq.com>
2022-06-02 09:09:24 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
-     
+     <Header></Header>
+     <Footer></Footer>
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
   name: 'App',
   components: {
-          
+      Header,
+      Footer   
   }
 }
 </script>
