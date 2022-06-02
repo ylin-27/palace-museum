295da42bca7e9b85c00832d61b1fa32fcc75d905
ylin-27 <260671287@qq.com>
2022-06-02 09:05:18 +0800

    master

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//引入swiper样式
+import "swiper/css/swiper.min.css";
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
